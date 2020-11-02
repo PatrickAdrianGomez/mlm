deb7d34b777798e7728b8d840e4792d2d8d9abce
Patrick Adrian Gómez Beltrán <patrick_adrian@outlook.com>
2020-11-02 01:02:26 -0400

    cambios profundos

--- entregas/routes/routesManager.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const complexRoutes = require('./complexRoutes');
-var routes = require('./routes');
-
-router.use('/v1', complexRoutes);
-router.use('/v1', routes);
-
-module.exports = router;
