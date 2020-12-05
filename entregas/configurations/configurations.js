ccd54b6e4b2f94860a2fdbdf96baaac45b5d1e09
Patrick Adrian Gómez Beltrán <patrick_adrian@outlook.com>
2020-12-05 13:10:05 -0400

    cambio de ip y puerto a nuevo vps

--- entregas/configurations/configurations.js
+++ entregas/configurations/configurations.js
@@ -10,7 +10,7 @@ var config = {
             reconnectInterval: 500
         },
         //uri: 'mongodb://localhost:27017/mlm'
-        uri: 'mongodb://userENTREGAS:MPatrickLRobertoM77@34.67.218.15:8890/entregas'
+        uri: 'mongodb://userENTREGAS:MPatrickLRobertoM77@45.225.75.2:16677/entregas'
     },
     bcrypt: {
         saltRounds: 11
