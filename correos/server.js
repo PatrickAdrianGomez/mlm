b698f6d44e5e39a09979a48b3268aee0ce42f9fb
Patrick Adrian Gómez Beltrán <patrick_adrian@outlook.com>
2020-12-21 10:05:26 -0400

    sin inicio

--- correos/server.js
+++ correos/server.js
@@ -1,35 +1,6 @@
 var http = require("http");
 var nodemailer = require('nodemailer');
 
-inicio();
-
-function inicio() {
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'mlm.conexion@gmail.com',
-            pass: 'Patito2112*'
-        }
-    });
-    var mailOptions = {
-        from: 'mlm.conexion@gmail.com',
-        to: 'patrick_adrian@hotmail.com',
-        subject: 'Bienvenido a MLM',
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.send(500, error.message);
-        } else {
-            console.log('Email enviado: ' + info.response);
-            res.status(200).jsonp(req.body);
-        }
-    });
-}
-
 http.createServer(function(request, response) {
 
     var transporter = nodemailer.createTransport({
@@ -42,7 +13,7 @@ http.createServer(function(request, response) {
     });
     var mailOptions = {
         from: 'mlm.conexion@gmail.com',
-        to: 'robertoirusta@gmail.com',
+        to: 'patrick_adrian@hotmail.com',
         subject: 'Bienvenido a MLM',
         text: "Hello world?", // plain text body
         html: "<b>Hello world?</b>", // html body
