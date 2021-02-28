7a6815cd487977d03ee524d4c6bac84cdf83e95e
Patrick Adrian Gómez Beltrán <patrick_adrian@outlook.com>
2021-02-28 17:55:32 -0400

    nuevo cambio

--- entregas/entregas.js
+++ entregas/entregas.js
@@ -40,7 +40,9 @@ app.use(express.json({limit: '100mb', extended: true}));//remplaza a body-parser
 // Inicialización de las rutas
 app.use('/api', routesManager);
 
-/** Estas lineas debo eliminar */
+/** Estas lineas debo eliminar
+ * 
+ */
 var ww = require('./configurations/webApiWhatsApp');
 ww.conectWhtspp();
 
